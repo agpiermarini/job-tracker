039e4446257b61213b78a27a3596ecafe7e6e35d
agpiermarini <piermarini.andrew@gmail.com>
2018-03-24 17:55:09 -0600

    Add jobs association to category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,27 +2,10 @@ require 'rails_helper'
 
 describe Category do
   describe "validations" do
-    context "invalid attributes" do
-      it "is invalid without a title" do
-        category = Category.new
-        expect(category).to be_invalid
-      end
-    end
+    it {is_expected.to validate_presence_of(:title)}
+  end
 
-    context "valid attributes" do
-      it "is valid with a title" do
-        category = Category.new(title: "Software")
-        expect(category).to be_valid
-      end
-    end
+  describe "relationships" do
+    it {is_expected.to have_many(:jobs)}
   end
 end
-
-
-
-  # describe "relationships" do
-  #   it "belongs to a company" do
-  #     job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
-  #     expect(job).to respond_to(:company)
-  #   end
-  # end
