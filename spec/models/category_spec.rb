64b246a6bf8a86a1f596d0838d73067042cafc71
agpiermarini <piermarini.andrew@gmail.com>
2018-03-24 17:48:18 -0600

    Add categories migration and model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,15 +5,24 @@ describe Category do
     context "invalid attributes" do
       it "is invalid without a title" do
         category = Category.new
-        expect(job).to be_invalid
+        expect(category).to be_invalid
       end
     end
 
     context "valid attributes" do
       it "is valid with a title" do
         category = Category.new(title: "Software")
-        expect(job).to be_valid
+        expect(category).to be_valid
       end
     end
   end
 end
+
+
+
+  # describe "relationships" do
+  #   it "belongs to a company" do
+  #     job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+  #     expect(job).to respond_to(:company)
+  #   end
+  # end
