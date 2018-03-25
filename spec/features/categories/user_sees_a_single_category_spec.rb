94c136a27ebc2e8de53428f7853043241267e8fc
agpiermarini <piermarini.andrew@gmail.com>
2018-03-25 00:26:04 -0600

    Scope job listings on categories show to list only jobs matching category id

--- spec/features/categories/user_sees_a_single_category_spec.rb
+++ spec/features/categories/user_sees_a_single_category_spec.rb
@@ -4,16 +4,19 @@ describe 'User visits page for a single category' do
   describe 'they visit a category\'s page' do
     it 'they see all jobs listed for that category' do
       company = Company.create!(name: "Google")
-      category = Category.create!(title: "User Experience")
-      job_1 = company.jobs.create(title: "User Experience Researcher I", level_of_interest: 5, city: "Boulder", category_id: category.id)
-      job_2 = company.jobs.create(title: "User Experience Researcher II", level_of_interest: 5, city: "Boulder", category_id: category.id)
-      job_1 = company.jobs.create(title: "Recruiter", level_of_interest: 5, city: "Boulder", category_id: category.id)
+      category_1 = Category.create!(title: "User Experience")
+      category_2 = Category.create!(title: "Software")
+      job_1 = company.jobs.create(title: "User Experience Researcher I", level_of_interest: 5, city: "Boulder", category_id: category_1.id)
+      job_2 = company.jobs.create(title: "User Experience Researcher II", level_of_interest: 5, city: "Boulder", category_id: category_1.id)
+      job_3 = company.jobs.create(title: "Recruiter", level_of_interest: 5, city: "Boulder", category_id: category_1.id)
+      job_4 = company.jobs.create(title: "Software Developer", level_of_interest: 5, city: "Boulder", category_id: category_2.id)
 
-      visit category_path(category)
+      visit category_path(category_2)
 
       expect(page).to have_content("User Experience Researcher I")
       expect(page).to have_content("User Experience Researcher II")
       expect(page).to have_content("Recruiter")
+      expect(page).to_not have_content("Software Developer")
     end
   end
 end
