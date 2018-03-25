5b578854def61660a9bcb6ce1485ba5bf7dbc2d5
Evan Wheeler <anon0mys87@gmail.com>
2018-03-24 18:55:34 -0600

    Preliminary test for creating a contact on a company page

---
+++ spec/features/contacts/user_creates_a_contact_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'when user is on a company show page' do
+  context 'clicking on add contact' do
+    scenario 'should create a new contact on company page' do
+      visit new_contact_path
+
+      fill_in 'contact[name]', with: 'Tom'
+      fill_in 'contact[role]', with: 'Manager'
+      fill_in 'contact[email]', with: 'tom@company.com'
+      click_button 'Save'
+
+      expect(current_path).to eq('/companies/:id')
+      within '.contacts-list' do
+        expect(page).to have_content('Tom')
+        expect(page).to have_content('Manager')
+        expect(page).to have_content('tom@company.com')
+      end
+    end
+  end
+end
