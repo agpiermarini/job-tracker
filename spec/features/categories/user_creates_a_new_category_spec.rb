2f19f326f8569be07cefc5c45014ff3b970f32ca
agpiermarini <piermarini.andrew@gmail.com>
2018-03-24 19:59:09 -0600

    Add title uniqueness validation

--- spec/features/categories/user_creates_a_new_category_spec.rb
+++ spec/features/categories/user_creates_a_new_category_spec.rb
@@ -12,4 +12,19 @@ describe 'User creates a new category' do
     expect(page).to have_content("You created the #{Category.last.title} Category")
     expect(page).to have_content("Category: #{Category.last.title}")
   end
+
+
+  context 'a user tries to create category that already exists' do
+    it 'they are directed to the new category form' do
+      @category = Category.create!(title: "Software")
+      visit new_category_path
+
+      fill_in 'category[title]', with: "Software"
+
+      click_button "Create"
+
+      expect(current_path).to eq(new_category_path)
+      expect(page).to have_content("Failed to create category")
+    end
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Category do
   describe "validations" do
     it {is_expected.to validate_presence_of(:title)}
+    it {is_expected.to validate_uniqueness_of(:title)}
   end
 
   describe "relationships" do
