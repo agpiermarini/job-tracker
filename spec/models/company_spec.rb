6c19ac09bf21e0ec33169f3259ba8da45cf8673f
agpiermarini <piermarini.andrew@gmail.com>
2018-03-25 00:18:40 -0600

    Add #jobs_by_category

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -29,4 +29,21 @@ describe Company do
       expect(company).to respond_to(:jobs)
     end
   end
+
+  describe "instance methods" do
+    describe ".jobs_by_category" do
+      it "returns all jobs matching a category_id" do
+        company = Company.create!(id: 1, name: "Dropbox")
+        Category.create!(id: 1, title: "Test_Category_1")
+        Category.create!(id: 2, title: "Test_Category_2")
+        job_1 = Job.create!(title: "Job 1", description: "Job 1", level_of_interest: 1, city: "Job 1", company_id: 1, category_id: 1)
+        job_2 = Job.create!(title: "Job 2", description: "Job 2", level_of_interest: 1, city: "Job 2", company_id: 1, category_id: 1)
+        job_3 = Job.create!(title: "Job 3", description: "Job 3", level_of_interest: 1, city: "Job 3", company_id: 1, category_id: 1)
+        job_4 = Job.create!(title: "Job 4", description: "Job 4", level_of_interest: 1, city: "Job 4", company_id: 1, category_id: 2)
+
+        expect(company.jobs_by_category(1).size).to eq(3)
+        expect(company.jobs.size).to eq(4)
+      end
+    end
+  end
 end
