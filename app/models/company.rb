f535024dc4542d0bb5c58d95a27cdb7401719656
Evan Wheeler <anon0mys87@gmail.com>
2018-03-26 23:21:12 -0600

    Created contact model, added contact handling login in company controller, updated company show view for contact create form

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,7 @@
 class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :jobs, dependent: :destroy
+  has_many :contacts, dependent: :destroy
 
   def jobs_by_category(category_id)
     jobs.where(category_id: category_id)
