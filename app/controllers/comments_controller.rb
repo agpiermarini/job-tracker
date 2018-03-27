84d2e06fe5aa601a5196eafc671e7d630a57d302
agpiermarini <piermarini.andrew@gmail.com>
2018-03-27 02:27:03 -0600

    Add create comment functionality. Update job controller and view to handle comment creation and comment display

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,15 @@ class CommentsController < ApplicationController
 
   def create
     binding.pry
-    @comment = Comment.create(params[:comment])
+    @job = Job.find(params[:job_id])
+    @comment = @job.comments.create(comment_params)
     flash[:success] = "You added a new comment to #{@job.title} at #{@job.company.name}"
-    redirect_to job_path(params[:id])
+    redirect_to job_path(params[:job_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
--- spec/features/comments/user_creates_a_new_comment_spec.rb
+++ spec/features/comments/user_creates_a_new_comment_spec.rb
@@ -5,7 +5,7 @@ describe 'User' do
     scenario 'and creates a new comment' do
       company = Company.create!(id: 2, name: "Dropbox")
       Category.create!(id: 1, title: "Developer")
-      job = company.jobs.create!(title: "Game Dev", description: "Great job", level_of_interest: 60, city: "Albuquerque", category_id: 1)
+      job = company.jobs.create!(id: 1, title: "Game Dev", description: "Great job", level_of_interest: 60, city: "Albuquerque", category_id: 1)
 
       visit job_path(job)
 
@@ -16,6 +16,24 @@ describe 'User' do
       expect(current_path).to eq("/jobs/#{Job.last.id}")
       expect(page).to have_content("You added a new comment to #{job.title} at #{job.company.name}")
       expect(page).to have_content("This looks like a great position.")
+    end
+  end
+
+  describe 'goes to a job page' do
+    scenario 'and sees all comments for that job' do
+      company = Company.create!(id: 2, name: "Dropbox")
+      Category.create!(id: 1, title: "Developer")
+      job = company.jobs.create!(id: 1, title: "Game Dev", description: "Great job", level_of_interest: 60, city: "Albuquerque", category_id: 1)
+      comment_1 = job.comments.create(content: "Comment 1")
+      comment_2 = job.comments.create(content: "Comment 2")
+      comment_3 = job.comments.create(content: "Comment 2")
+
+      visit job_path(job)
+      save_and_open_page
+
+      expect(page).to have_content(comment_1.content)
+      expect(page).to have_content(comment_2.content)
+      expect(page).to have_content(comment_3.content)
     end
   end
 end
