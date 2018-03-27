d50668cb8156724724c52c684c44809a9089c1ec
Evan Wheeler <anon0mys87@gmail.com>
2018-03-27 16:47:03 -0600

    User can add contacts on company show page and see a list

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,6 @@ class ContactsController < ApplicationController
   def create
     company = Company.find(params[:company_id])
     company.contacts.create(contact_params)
-    binding.pry
     redirect_to company_path(company)
   end
 
