7e70e4d70c7bf4abfb74b1cc8e8dbe09e6eb4212
Evan Wheeler <anon0mys87@gmail.com>
2018-03-27 01:07:37 -0600

    Tests for filter jobs by category and location complete

--- spec/features/jobs/user_can_sort_and_filter_jobs_spec.rb
+++ spec/features/jobs/user_can_sort_and_filter_jobs_spec.rb
@@ -1,25 +1,35 @@
 require 'rails_helper'
 
 describe 'User' do
-  DatabaseCleaner.clean
-  @company_one = Company.create!(name: "ESPN")
-  @company_two = Company.create!(name: "Google")
-  @category_one = Category.create!(title: "Media")
-  @category_two = Category.create!(title: "Big Data")
-  @job_one = @company_one.jobs.create!(title: "Title 1", description: "This is Job 1", level_of_interest: 5, city: "Denver")
-  @job_two = @company_one.jobs.create!(title: "Title 2", description: "This is Job 2", level_of_interest: 4, city: "Denver")
-  @job_three = @company_two.jobs.create!(title: "Title 3", description: "This is Job 3", level_of_interest: 3, city: "LA")
-  @job_four = @company_two.jobs.create!(title: "Title 4", description: "This is Job 4", level_of_interest: 2, city: "Chicago")
+  before(:each) do
+    DatabaseCleaner.clean
+    @company_one = Company.create!(name: "ESPN")
+    @company_two = Company.create!(name: "Google")
+    @category_one = Category.create!(title: "Media")
+    @category_two = Category.create!(title: "Development")
+    @job_one = @company_one.jobs.create!(title: "Title 1", description: "This is Job 1", level_of_interest: 5, city: "Denver", category_id: @category_one.id)
+    @job_two = @company_one.jobs.create!(title: "Title 2", description: "This is Job 2", level_of_interest: 4, city: "Denver", category_id: @category_one.id)
+    @job_three = @company_two.jobs.create!(title: "Title 3", description: "This is Job 3", level_of_interest: 3, city: "LA", category_id: @category_one.id)
+    @job_four = @company_two.jobs.create!(title: "Title 4", description: "This is Job 4", level_of_interest: 2, city: "Chicago", category_id: @category_two.id)
+  end
 
   context 'can filter jobs' do
     scenario 'by location' do
-      visit jobs_path
-      click_on 'location'
+      visit '/jobs?location=Denver'
 
+      expect(page).to have_content(@job_one.title)
+      expect(page).to have_content(@job_two.title)
+      expect(page).to_not have_content(@job_three.title)
+      expect(page).to_not have_content(@job_four.title)
     end
 
     scenario 'by category' do
+      visit '/jobs?category=Development'
 
+      expect(page).to have_content(@job_one.title)
+      expect(page).to have_content(@job_two.title)
+      expect(page).to have_content(@job_three.title)
+      expect(page).to_not have_content(@job_four.title)
     end
   end
 
